7850db81a6f65915aad858c3af0db42c7894f0a9
Simon Chervenak <sarras305@gmail.com>
2020-04-24 17:01:43 -0400

    thinkin better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from enum import Enum
 '''
 lowercase is yellow, uppercase is red
 Ss: square, Tt: triangle, Cc: circle
@@ -26,11 +26,26 @@ BOARD = '\n'.join([
     '..............',
     '..............', 
 ])
+SQUARES = 'SCT'
+
+class PieceType(Enum):
+    SQUARE, CIRCLE, TRIANGLE = SQUARES
+
+class Color(Enum):
+    YELLOW, RED = range(2)
 
 class Piece:
     def __init__(self, master, typ):
-        ...
+        self.master = master
+        self.typ = typ
+        self.color = Color.YELLOW if typ == typ.upper() else Color.RED
 
 class Board:
     def __init__(self):
-        self.board = 
+        self.pieces = []
+
+        # initialize pieces
+        for row in BOARD:
+            for sq in row:
+                if sq.upper() in SQUARES:
+                    self.pieces.append(Piece(self, sq))
