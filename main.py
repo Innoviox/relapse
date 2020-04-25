b5780fc24ee2fedc8a70321d0447187999ea8a86
Simon Chervenak <sarras305@gmail.com>
2020-04-24 20:38:38 -0400

    trying movement

--- main.py
+++ main.py
@@ -78,19 +78,26 @@ class Board(tk.Tk):
     def clicked(self, event):
         label = event.widget
 
-        if self.selected:
-            ...
+        if self.selected and label in self.highlighted:
+            self.move_to(label)
+            self.clear_highlighted()
+            self.selected = None
+            # todo: tick turn?
         elif label['text'].upper() in SQUARES:
             self.clear_highlighted()
             self.highlight_possible(label)
+            self.selected = label
+
+    def move_to(self, label): # note: assumes selected has been set
+        label.config(text=self.selected['text'], bg=self.selected['bg'])
+        self.selected['text'] = ' '
+        self.reset(self.selected)
 
     def highlight_possible(self, label): # note: assumes clear_highlighted has already been called
-        info = label.master.grid_info()
-        r, c = info['row'], info['column']
+        r, c = self.get_pos(label)
         
         for label in self.valid_moves(label['text'], (r, c)):
             self.highlight(label)
-            
 
     def valid_moves(self, typ, pos):
         r, c = pos
@@ -109,14 +116,22 @@ class Board(tk.Tk):
 
     def clear_highlighted(self):
         for sq in self.highlighted:
-            sq.config(bg=COLORS[sq['text']])
+            self.reset(sq)
         self.highlighted = []
+
+    def reset(self, label): # reset to original color
+        r, c = self.get_pos(label)
+        label.config(bg=COLORS[BOARD[r][c]])
 
     def label_at(self, row, col):
         try:
             return self.board_frame.grid_slaves(row, col)[0].winfo_children()[0]
         except (tk.TclError, IndexError): # went off grid
             return None
+
+    def get_pos(self, label):
+        info = label.master.grid_info()
+        return info['row'], info['column']
             
 
 if __name__ == "__main__":
